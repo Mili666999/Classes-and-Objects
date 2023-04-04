b6af1be64767b61cc0e05c720f90d43b74c67763
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-04-05 00:21:06 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //create a loading object with the same name
+
+        loading.LoadingMessage();
+    }
+}
+
+class Loading {
+    //complete the class, add LoadingMessenger() method
+
+}
